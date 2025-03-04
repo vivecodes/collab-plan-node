705792c0bad74333c8e8cf2e720bf04f72d380d7
Vira Velmozhna <viravelmozhna@gmail.com>
2025-03-04 22:02:03 +0100

    update cors for allowing preflight options method

--- server.js
+++ server.js
@@ -19,7 +19,15 @@ const io = new Server(server, {
 });
 
 app.use(express.json());
-app.use(cors({ origin: FRONTEND_URL, credentials: true }));
+app.use(
+  cors({
+    origin: FRONTEND_URL,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
+app.options("*", cors());
 
 mongoose
   .connect(DB_HOST)
